bd2e1dae6d75b484fa27ccde330aad9c09a1088e
marcosdary <programadordarkness2004@gmail.com>
2026-06-12 09:47:40 -0300

    Add socket para testar conexão

--- backend/src/api/v1/__init__.py
+++ backend/src/api/v1/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
 
-from src.api.v1.routers import cv
+from src.api.v1.routers import cv, ws
 
 router = APIRouter()
 
 
 router.include_router(cv.router, prefix="/cv")
+router.include_router(ws.router, prefix="/ws")
+
 
 
---
+++ backend/src/api/v1/routers/ws.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+router = APIRouter()
+
+@router.websocket("/health")
+async def websocket_health(websocket: WebSocket):
+    await websocket.accept()
+
+    try:
+        while True:
+            data = await websocket.receive_json()
+            if data["type"] == "ping":
+                await websocket.send_json({
+                    "type": "pong"
+                })
+
+    except WebSocketDisconnect:
+        print("Cliente desconectado")
